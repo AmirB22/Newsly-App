bd305abcdbdccc7b1618611c0ca57d1fe05aab11
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-01-19 23:22:10 +0100

    style: news page buttons look nicer

--- newsPage.js
+++ newsPage.js
@@ -1,3 +1,15 @@
 let Logged = JSON.parse(localStorage.getItem("logged")) || false;
 
 if (!Logged) document.body.innerHTML = `<p>You are not logged in</p>`;
+
+document.querySelectorAll("li").forEach((el) => {
+  el.classList.add("list-unclicked");
+  el.addEventListener("click", function (e) {
+    document.querySelectorAll("li").forEach((el) => {
+      el.classList.add("list-unclicked");
+      el.classList.remove("list-clicked");
+    });
+    e.target.classList.remove("list-unclicked");
+    e.target.classList.add("list-clicked");
+  });
+});
